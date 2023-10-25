8a381ee18520900beb73a4ab97d6ce9115a6d3df
t112310235 <146065142+t112310235@users.noreply.github.com>
2023-10-25 11:46:39 +0800

    Update hw4-3.c

--- hw4-3.c
+++ hw4-3.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+
+    scanf("%d", &N);
+for (int i = 0; i < N; i++) {
+        int s[i], d[i];
+
+        scanf("%d %d", &a[i], &d[i]);
+
+}
+for (int i = 0; i < N; i++) {
 
