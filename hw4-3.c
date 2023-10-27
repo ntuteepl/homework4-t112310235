b5bd1b4a2f95d511b652257644648bff21ee1338
t112310235 <146065142+t112310235@users.noreply.github.com>
2023-10-27 21:30:35 +0800

    Update hw4-3.c

--- hw4-3.c
+++ hw4-3.c
@@ -5,15 +5,19 @@
 int main() {
     int N;
     scanf("%d", &N);
-
-    int s[N], d[N];
+    int sd[N][2];
+   /* int s[N], d[N];
 
     for (int i = 0; i < N; i++) {
         scanf("%d %d", &s[i], &d[i]);
+    }*/
+     for (int i = 0; i < N; i++) {
+        scanf("%d %d", &sd[i][0], &sd[i][1]);
     }
 
+
     // 使用冒泡排序对订单数据按开始时间 s 进行排序
-    for (int i = 0; i < N - 1; i++) {
+   /* for (int i = 0; i < N - 1; i++) {
         for (int j = 0; j < N - i - 1; j++) {
             if (s[j] > s[j + 1] || (s[j] == s[j+1] && d[j] > d[j+1])) {
                 int temp = s[j];
@@ -26,12 +30,51 @@ int main() {
         }
     }
     }
+    */
+      for (int i = 0; i < N - 1; i++) {
+        for (int j = 0; j < N - i - 1; j++) {
+            if (sd[j][0] > sd[j + 1][0]||(sd[j][0] == sd[j + 1][0] && sd[j][1] > sd[j+1][1])) {
+                // Swap the rows if the first column element is out of order
+                int temp1 = sd[j][0];
+                int temp2 = sd[j][1];
+                sd[j][0] = sd[j + 1][0];
+                sd[j][1] = sd[j + 1][1];
+                sd[j + 1][0] = temp1;
+                sd[j + 1][1] = temp2;
+            }
+        }
+    }
+        int cars=0;
+
+        for (int i=0;i<N;i++){
+            for (int j=0;j<N;j++){
+                if(sd[i][0]!=0&&sd[i][1]!=0){
+                    if(j<i)  {
+                        j=i+1;
+                    }
+
+        if(sd[i][0]==sd[j][0]){
+            cars++;
+
+            break;
+        }
+        //   開始<結束
+        if(sd[j+1][0]<sd[i][1]){
 
 
+                cars++;
+        }
+        //    開始>結束
+         if(sd[j+1][0]>=sd[i][1]){
+                cars=cars;
+        }
 
-    int cars;
-    for (int i=0;i<N;i++){
-    for (int j=0;j<N;j++){
+    }
+}
+}
+
+  /* for (int i=0;i<N;i++){
+        for (int j=0;j<N;j++){
          if(j<i)  {
             j=i+1;
          }
@@ -48,24 +91,29 @@ int main() {
          if(s[j+1]>=d[i]){
                 cars=cars;
         }
-        
+
 
     }
-    }
+    }*/
 
-        printf("%d\n", cars);
+        printf("%d\n", cars+1);
         int driver;
         for(driver=1;driver <= cars;driver++){
               printf("Driver %d's schedule is", driver );
-              
+
         }
-        
-        
+
+
 
     // 输出排序后的订单数据
-    for (int i = 0; i < N; i++) {
+   /* for (int i = 0; i < N; i++) {
         printf("%d %d\n", s[i], d[i]);
     }
+    */
+    for (int i = 0; i < N; i++) {
+        printf("%d %d\n", sd[i][0], sd[i][1]);
+    }
+
 
     return 0;
 }
