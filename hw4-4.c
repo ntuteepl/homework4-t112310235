119e624a12b020d7e84c7453bbfccd07138cd11a
t112310235 <146065142+t112310235@users.noreply.github.com>
2023-10-30 00:08:51 +0800

    Update hw4-4.c

--- hw4-4.c
+++ hw4-4.c
@@ -1,47 +1,44 @@
-#define ARRAYSIZE 100
 
 #include <stdio.h>
 
 int main() {
-    int n = 0;
-    int sd[ARRAYSIZE][2];
-    while (scanf("%d %d", &sd[n][0], &sd[n][1]) != EOF) {
-        n++;
-    }
+    int n;
 
-    // 冒泡排序
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (sd[j][0] > sd[j + 1][0] || (sd[j][0] == sd[j + 1][0] && sd[j][1] > sd[j + 1][1])) {
-                // Swap the rows if the first column element is out of order
-                int temp1 = sd[j][0];
-                int temp2 = sd[j][1];
-                sd[j][0] = sd[j + 1][0];
-                sd[j][1] = sd[j + 1][1];
-                sd[j + 1][0] = temp1;
-                sd[j + 1][1] = temp2;
-            }
+    scanf("%d", &n);
+    int matrix[n][n];
+    int boy=0, girl=0,max=0;
+  for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
         }
     }
 
-    int cars = 0;
-    for (int i = 0; i < n-1; i++) {
-        if (sd[i][0] != 0) { // Skip already processed rows
-            cars++;
-            printf("Driver %d's schedule is %d %d ", cars, sd[i][0], sd[i][1]);
+for(int c=0;c<n;c++)
+{
 
-            for (int j = i + 1; j < n; j++) {
-                if (sd[j][0] >= sd[i][1]) {
-                    // 当前任务可以分配给当前司机
-                    printf("%d %d ", sd[j][0], sd[j][1]);
-                    sd[i][1] = sd[j][1]; // 更新当前司机的结束时间
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(matrix[i][j]>max){
+
+                max=matrix[i][j];
+                boy=i;
+                girl=j;
+
                 }
-            }
-            printf("\n");
-        }
-    }
+                }
 
-    printf("%d\n", cars);
+            }
+for(int i=0;i<n;i++)
+{
+ matrix[boy][i]=0;
+ matrix[i][girl]=0;
+}
+
+
+printf("boy %d pair with girl %d.\n",boy+1,girl+1);
+max=0;
+}
 
     return 0;
+
 }
